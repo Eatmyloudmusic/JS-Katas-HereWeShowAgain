e082988339616d7ba1d915232519522148846fd5
william_michael_brough <eatmyloudmusic@gmail.com>
2020-06-15 18:52:19 -0400

    forgot to cite other members. lol.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// this code was contributed by: me and...
+//   Jessica Ribbecke
+//     Phillip Golden
+//       and Prince Adom
 
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
